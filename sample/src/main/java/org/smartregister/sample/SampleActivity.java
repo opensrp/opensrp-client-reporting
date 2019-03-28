84883a89f1d34f02e2a7d508ef07f155545865f1
Allan <a.onchuru@gmail.com>
2019-03-28 11:27:14 +0300

    Add HelloCharts dependency
    Added the charting library dependency for use in displaying various indicators

--- sample/src/main/java/org/smartregister/sample/SampleActivity.java
+++ sample/src/main/java/org/smartregister/sample/SampleActivity.java
@@ -3,7 +3,7 @@ package org.smartregister.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
