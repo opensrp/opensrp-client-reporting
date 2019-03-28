f96f276b4323bfc052f540ddcc97c59dbd904baf
Allan <a.onchuru@gmail.com>
2019-03-28 17:54:39 +0300

    Add get numeric indicator factory implementation and styling
    Added implementation for getting the numeric indicator view. Also added styling for the various labels

--- opensrp-reporting/src/main/java/org/smartregister/reporting/models/NumericIndicatorFactory.java
+++ opensrp-reporting/src/main/java/org/smartregister/reporting/models/NumericIndicatorFactory.java
@@ -1,15 +1,27 @@
 package org.smartregister.reporting.models;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import org.smartregister.reporting.R;
 import org.smartregister.reporting.interfaces.CommonReportingVisualisationListener;
 import org.smartregister.reporting.interfaces.IndicatorVisualisationFactory;
 
 public class NumericIndicatorFactory implements IndicatorVisualisationFactory {
     @Override
     public View getIndicatorView(ReportingIndicatorData data, Context context, CommonReportingVisualisationListener listener) {
-        // TODO :: Return Numeric indicator view
-        return null;
+        NumericIndicatorData indicatorData = (NumericIndicatorData) data;
+
+        RelativeLayout rootLayout = (RelativeLayout) LayoutInflater.from(context).inflate(R.layout.numeric_indicator_view, null);
+
+        TextView chartLabelTextView = rootLayout.findViewById(R.id.numeric_indicator_label);
+        TextView chartValueTextView = rootLayout.findViewById(R.id.numeric_indicator_value);
+        chartLabelTextView.setText(indicatorData.getIndicatorLabel());
+        chartValueTextView.setText(String.valueOf(indicatorData.getValue()));
+
+        return rootLayout;
     }
 }
