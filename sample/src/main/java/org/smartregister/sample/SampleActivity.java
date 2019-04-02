10c8bf14932c6568034c480cf6bfecb84558da3b
Allan <a.onchuru@gmail.com>
2019-04-02 12:52:50 +0300

    Refactor Sample app to display in fragment
    Refactored the Sample app to show the sample pie chart and numeric indicator in a fragment

--- sample/src/main/java/org/smartregister/sample/SampleActivity.java
+++ sample/src/main/java/org/smartregister/sample/SampleActivity.java
@@ -1,84 +1,65 @@
 package org.smartregister.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
-import org.smartregister.reporting.interfaces.PieChartSelectListener;
-import org.smartregister.reporting.model.NumericDisplayFactory;
-import org.smartregister.reporting.model.NumericIndicatorVisualization;
-import org.smartregister.reporting.model.PieChartIndicatorData;
-import org.smartregister.reporting.model.PieChartFactory;
-import org.smartregister.reporting.model.PieChartIndicatorVisualization;
-import org.smartregister.reporting.model.PieChartSlice;
-import org.smartregister.sample.utils.ChartUtil;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class SampleActivity extends AppCompatActivity {
-
-    private View pieChartView;
-    private View numericIndicatorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_activity);
 
-        generateCharts();
-
-        ViewGroup viewGroup = findViewById(R.id.content);
-        viewGroup.addView(numericIndicatorView);
-        viewGroup.addView(pieChartView);
+        setUpViews();
     }
 
-    private void generateCharts() {
+    private void setUpViews() {
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(" ");
+        }
 
-        // Generate pie chart
-        PieChartIndicatorVisualization pieChartIndicatorVisualization = new PieChartIndicatorVisualization();
-        pieChartIndicatorVisualization.setIndicatorLabel(getResources().getString(R.string.num_of_lieterate_children_0_60_label));
+        // Adapter that will return a fragment for a section of the Activity
+        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        PieChartIndicatorData chartData = new PieChartIndicatorData();
-        chartData.setHasLabels(true);
-        chartData.setHasLabelsOutside(true);
-        chartData.setHasCenterCircle(false);
+        ViewPager mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        List<PieChartSlice> slices = new ArrayList<>();
+        TabLayout tabLayout = findViewById(R.id.tabs);
 
-        int yesColor = ChartUtil.YES_GREEN_SLICE_COLOR;
-        int noColor = ChartUtil.NO_RED_SLICE_COLOR;
-        PieChartSlice yesSlice = new PieChartSlice(58, yesColor);
-        PieChartSlice noSlice = new PieChartSlice(42, noColor);
-
-        slices.add(yesSlice);
-        slices.add(noSlice);
-
-        chartData.setSlices(slices);
-        chartData.setListener(new ChartListener());
-
-        pieChartIndicatorVisualization.setChartData(chartData);
-
-        PieChartFactory pieChartFactory = new PieChartFactory();
-        pieChartView = pieChartFactory.getIndicatorView(pieChartIndicatorVisualization, this);
-
-        // Generate numeric indicator
-        NumericIndicatorVisualization numericIndicatorData = new NumericIndicatorVisualization(
-                getResources().getString(R.string.total_under_5_count), 199);
-
-        NumericDisplayFactory numericIndicatorFactory = new NumericDisplayFactory();
-        numericIndicatorView = numericIndicatorFactory.getIndicatorView(numericIndicatorData, this);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
     }
 
-    private class ChartListener implements PieChartSelectListener {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
         @Override
-        public void handleOnSelectEvent(PieChartSlice sliceValue) {
-            Toast.makeText(getApplicationContext(), ChartUtil.getPieSelectionValue(sliceValue, getApplicationContext()), Toast.LENGTH_SHORT).show();
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return DashboardFragment.newInstance();
+                case 1:
+                    return ResourcesFragment.newInstance();
+                default:
+                    return ResourcesFragment.newInstance();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
         }
     }
-
 
 }
