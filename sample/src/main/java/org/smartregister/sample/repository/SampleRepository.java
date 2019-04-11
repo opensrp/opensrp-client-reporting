95a1462e8bf82d0708630ab68a4b4da218d48c4c
Allan <a.onchuru@gmail.com>
2019-04-11 11:06:45 +0300

    Add SampleRepository repositories initialisation

--- sample/src/main/java/org/smartregister/sample/repository/SampleRepository.java
+++ sample/src/main/java/org/smartregister/sample/repository/SampleRepository.java
@@ -1,14 +1,25 @@
 package org.smartregister.sample.repository;
 
 import android.content.Context;
+import android.util.Log;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
 import org.smartregister.AllConstants;
+import org.smartregister.reporting.repository.DailyIndicatorCountRepository;
+import org.smartregister.reporting.repository.IndicatorQueryRepository;
+import org.smartregister.reporting.repository.IndicatorRepository;
+import org.smartregister.repository.EventClientRepository;
 import org.smartregister.repository.Repository;
 
 
 public class SampleRepository extends Repository {
+
+    protected SQLiteDatabase readableDatabase;
+    protected SQLiteDatabase writableDatabase;
+    private String databasePassword = "db_pass";
+    private static final String TAG = SampleRepository.class.getCanonicalName();
+
 
     public SampleRepository(Context context, org.smartregister.Context openSRPContext) {
         super(context, AllConstants.DATABASE_NAME, AllConstants.DATABASE_VERSION, openSRPContext.session(), null, openSRPContext.sharedRepositoriesArray());
@@ -16,6 +27,66 @@ public class SampleRepository extends Repository {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
+        EventClientRepository.createTable(database, EventClientRepository.Table.client, EventClientRepository.client_column.values());
+        EventClientRepository.createTable(database, EventClientRepository.Table.event, EventClientRepository.event_column.values());
+        IndicatorRepository.createTable(database);
+        IndicatorQueryRepository.createTable(database);
+        DailyIndicatorCountRepository.createTable(database);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    @Override
+    public SQLiteDatabase getReadableDatabase() {
+        return getReadableDatabase(databasePassword);
+    }
+
+    @Override
+    public SQLiteDatabase getWritableDatabase() {
+        return getWritableDatabase(databasePassword);
+    }
+
+    @Override
+    public synchronized SQLiteDatabase getReadableDatabase(String password) {
+        try {
+            if (readableDatabase == null || !readableDatabase.isOpen()) {
+                if (readableDatabase != null) {
+                    readableDatabase.close();
+                }
+                readableDatabase = super.getReadableDatabase(password);
+            }
+            return readableDatabase;
+        } catch (Exception e) {
+            Log.e(TAG, "Database Error. " + e.getMessage());
+            return null;
+        }
+
+    }
+
+    @Override
+    public synchronized SQLiteDatabase getWritableDatabase(String password) {
+        if (writableDatabase == null || !writableDatabase.isOpen()) {
+            if (writableDatabase != null) {
+                writableDatabase.close();
+            }
+            writableDatabase = super.getWritableDatabase(password);
+        }
+        return writableDatabase;
+    }
+
+    @Override
+    public synchronized void close() {
+        if (readableDatabase != null) {
+            readableDatabase.close();
+        }
+
+        if (writableDatabase != null) {
+            writableDatabase.close();
+        }
+        super.close();
+    }
+
+
 }
