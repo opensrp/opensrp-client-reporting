40e2232a4b9d25b6575a17818479072b34dd4d33
Allan <a.onchuru@gmail.com>
2019-04-18 13:08:46 +0300

    Add BaseUnitTest
    This is the base class with RoboElectric config initialization

---
+++ opensrp-reporting/src/test/java/org/smartregister/reporting/BaseUnitTest.java
@@ -0,0 +1,10 @@
+package org.smartregister.reporting;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 22)
+public class BaseUnitTest {
+}
