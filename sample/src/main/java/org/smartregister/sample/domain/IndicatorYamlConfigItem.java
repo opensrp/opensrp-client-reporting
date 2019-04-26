a7df827c2fabb3f3898f6f4e6606ee0024e1501b
Allan <a.onchuru@gmail.com>
2019-04-26 17:27:24 +0300

    Add SnakeYaml and indicator data config definition files

---
+++ sample/src/main/java/org/smartregister/sample/domain/IndicatorYamlConfigItem.java
@@ -0,0 +1,34 @@
+package org.smartregister.sample.domain;
+
+public class IndicatorYamlConfigItem {
+
+    public static String INDICATOR_PROPERTY = "indicatorData";
+
+    private String key;
+    private String description;
+    private String indicatorQuery;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIndicatorQuery() {
+        return indicatorQuery;
+    }
+
+    public void setIndicatorQuery(String indicatorQuery) {
+        this.indicatorQuery = indicatorQuery;
+    }
+}
---
+++ sample/src/main/java/org/smartregister/sample/domain/IndicatorsYamlConfig.java
@@ -0,0 +1,15 @@
+package org.smartregister.sample.domain;
+
+import java.util.List;
+
+public class IndicatorsYamlConfig {
+    private List<IndicatorYamlConfigItem> indicators;
+
+    public List<IndicatorYamlConfigItem> getIndicators() {
+        return indicators;
+    }
+
+    public void setIndicators(List<IndicatorYamlConfigItem> indicators) {
+        this.indicators = indicators;
+    }
+}
