b2f5f828517ddf850394145946f0cce7ae300d34
Elly Kitoto <ekitoto@ona.io>
2019-07-18 17:14:59 +0300

    Fixed codacy issue

--- opensrp-reporting/src/test/java/org/smartregister/reporting/ReportingLibraryTest.java
+++ opensrp-reporting/src/test/java/org/smartregister/reporting/ReportingLibraryTest.java
@@ -1,38 +1,26 @@
 package org.smartregister.reporting;
 
-import android.content.res.AssetManager;
-
-import net.sqlcipher.database.SQLiteDatabase;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.smartregister.Context;
 import org.smartregister.commonregistry.CommonFtsObject;
-import org.smartregister.repository.AllSharedPreferences;
 import org.smartregister.repository.Repository;
 
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(PowerMockRunner.class)
 public class ReportingLibraryTest {
 
     @Mock
     private Context context;
-
     @Mock
     private Repository repository;
-
     @Mock
     private CommonFtsObject commonFtsObject;
-
-    @Mock
-    private AllSharedPreferences allSharedPreferences;
-
-    @Mock
-    private SQLiteDatabase sqLiteDatabase;
-
-    @Mock
-    AssetManager assetManager;
 
     private int appVersion = 1;
     private int dbVersion = 1;
@@ -45,18 +33,17 @@ public class ReportingLibraryTest {
     @Test
     public void testInit() {
         ReportingLibrary.init(context, repository, commonFtsObject, appVersion, dbVersion);
-        Assert.assertNotNull(ReportingLibrary.getInstance());
+        assertNotNull(ReportingLibrary.getInstance());
     }
 
     @Test
     public void testThatAllRepositoriesAreInitialized() {
         ReportingLibrary.init(context, repository, commonFtsObject, appVersion, dbVersion);
         ReportingLibrary reportingLibrary = ReportingLibrary.getInstance();
-        Assert.assertNotNull(reportingLibrary.dailyIndicatorCountRepository());
-        Assert.assertNotNull(reportingLibrary.indicatorQueryRepository());
-        Assert.assertNotNull(reportingLibrary.eventClientRepository());
-        Assert.assertNotNull(reportingLibrary.dailyIndicatorCountRepository());
+        assertNotNull(reportingLibrary.dailyIndicatorCountRepository());
+        assertNotNull(reportingLibrary.indicatorQueryRepository());
+        assertNotNull(reportingLibrary.eventClientRepository());
+        assertNotNull(reportingLibrary.dailyIndicatorCountRepository());
 
     }
-
 }
