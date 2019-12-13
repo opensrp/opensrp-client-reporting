d6c004d104900b1500f73e214f29b690dc2456b1
rkodev <43806892+rkodev@users.noreply.github.com>
2019-12-13 22:30:46 +0300

    Added test

--- opensrp-reporting/src/main/java/org/smartregister/reporting/service/IndicatorGeneratorIntentService.java
+++ opensrp-reporting/src/main/java/org/smartregister/reporting/service/IndicatorGeneratorIntentService.java
@@ -11,6 +11,7 @@ import org.smartregister.reporting.event.IndicatorTallyEvent;
 import org.smartregister.reporting.repository.DailyIndicatorCountRepository;
 import org.smartregister.reporting.repository.IndicatorQueryRepository;
 import org.smartregister.reporting.repository.IndicatorRepository;
+import org.smartregister.repository.AllSharedPreferences;
 
 import timber.log.Timber;
 
@@ -25,36 +26,42 @@ public class IndicatorGeneratorIntentService extends IntentService {
 
     public static String TAG = "IndicatorGeneratorIntentService";
     private ReportIndicatorDaoImpl reportIndicatorDao;
+    private org.smartregister.repository.AllSharedPreferences allSharedPreferences;
 
     public IndicatorGeneratorIntentService() {
         super("IndicatorGeneratorIntentService");
     }
 
+    // TODO fix missing password on core
     @Override
     protected void onHandleIntent(Intent intent) {
-        Timber.i("IndicatorGeneratorIntentService running");
-        String lastProcessedDate = ReportingLibrary.getInstance().getContext().allSharedPreferences().getPreference(ReportIndicatorDaoImpl.REPORT_LAST_PROCESSED_DATE);
-        Timber.d("LastProcessedDate %s", lastProcessedDate);
-        IndicatorTallyEvent tallyEvent = new IndicatorTallyEvent();
-        tallyEvent.setStatus(TallyStatus.STARTED);
-        EventBusHelper.postEvent(tallyEvent);
-        reportIndicatorDao.generateDailyIndicatorTallies(lastProcessedDate);
-        tallyEvent.setStatus(TallyStatus.COMPLETE);
-        EventBusHelper.postEvent(tallyEvent);
+        try {
+            Timber.i("IndicatorGeneratorIntentService running");
+            String lastProcessedDate = allSharedPreferences.getPreference(ReportIndicatorDaoImpl.REPORT_LAST_PROCESSED_DATE);
+            Timber.d("LastProcessedDate %s", lastProcessedDate);
+            IndicatorTallyEvent tallyEvent = new IndicatorTallyEvent();
+            tallyEvent.setStatus(TallyStatus.STARTED);
+            EventBusHelper.postEvent(tallyEvent);
+            reportIndicatorDao.generateDailyIndicatorTallies(lastProcessedDate);
+            tallyEvent.setStatus(TallyStatus.COMPLETE);
+            EventBusHelper.postEvent(tallyEvent);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    public AllSharedPreferences getAllSharedPreferences() {
+        if(allSharedPreferences == null)
+            allSharedPreferences = ReportingLibrary.getInstance().getContext().allSharedPreferences();
+        return allSharedPreferences;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        try {
-            // TODO fix missing password on core
-            // generating indicators can cause errors
-            DailyIndicatorCountRepository dailyIndicatorCountRepository = ReportingLibrary.getInstance().dailyIndicatorCountRepository();
-            IndicatorQueryRepository indicatorQueryRepository = ReportingLibrary.getInstance().indicatorQueryRepository();
-            IndicatorRepository indicatorRepository = ReportingLibrary.getInstance().indicatorRepository();
-            reportIndicatorDao = new ReportIndicatorDaoImpl(indicatorQueryRepository, dailyIndicatorCountRepository, indicatorRepository);
-        } catch (Exception e) {
-            Timber.e(e);
-        }
+        DailyIndicatorCountRepository dailyIndicatorCountRepository = ReportingLibrary.getInstance().dailyIndicatorCountRepository();
+        IndicatorQueryRepository indicatorQueryRepository = ReportingLibrary.getInstance().indicatorQueryRepository();
+        IndicatorRepository indicatorRepository = ReportingLibrary.getInstance().indicatorRepository();
+        reportIndicatorDao = new ReportIndicatorDaoImpl(indicatorQueryRepository, dailyIndicatorCountRepository, indicatorRepository);
         return super.onStartCommand(intent, flags, startId);
     }
 }
