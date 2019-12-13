747d946872fbd9445f983c95faed9ed6a70e3f28
rkodev <43806892+rkodev@users.noreply.github.com>
2019-12-13 20:21:33 +0300

    Fix service dying

--- opensrp-reporting/src/main/java/org/smartregister/reporting/service/IndicatorGeneratorIntentService.java
+++ opensrp-reporting/src/main/java/org/smartregister/reporting/service/IndicatorGeneratorIntentService.java
@@ -2,7 +2,6 @@ package org.smartregister.reporting.service;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
 
 import org.smartregister.reporting.ReportingLibrary;
 import org.smartregister.reporting.dao.ReportIndicatorDaoImpl;
@@ -12,6 +11,8 @@ import org.smartregister.reporting.event.IndicatorTallyEvent;
 import org.smartregister.reporting.repository.DailyIndicatorCountRepository;
 import org.smartregister.reporting.repository.IndicatorQueryRepository;
 import org.smartregister.reporting.repository.IndicatorRepository;
+
+import timber.log.Timber;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -31,9 +32,9 @@ public class IndicatorGeneratorIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.i(TAG, "IndicatorGeneratorIntentService running");
+        Timber.i("IndicatorGeneratorIntentService running");
         String lastProcessedDate = ReportingLibrary.getInstance().getContext().allSharedPreferences().getPreference(ReportIndicatorDaoImpl.REPORT_LAST_PROCESSED_DATE);
-        Log.d(TAG, "LastProcessedDate " + lastProcessedDate);
+        Timber.d("LastProcessedDate %s", lastProcessedDate);
         IndicatorTallyEvent tallyEvent = new IndicatorTallyEvent();
         tallyEvent.setStatus(TallyStatus.STARTED);
         EventBusHelper.postEvent(tallyEvent);
@@ -44,10 +45,16 @@ public class IndicatorGeneratorIntentService extends IntentService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        DailyIndicatorCountRepository dailyIndicatorCountRepository = ReportingLibrary.getInstance().dailyIndicatorCountRepository();
-        IndicatorQueryRepository indicatorQueryRepository = ReportingLibrary.getInstance().indicatorQueryRepository();
-        IndicatorRepository indicatorRepository = ReportingLibrary.getInstance().indicatorRepository();
-        reportIndicatorDao = new ReportIndicatorDaoImpl(indicatorQueryRepository, dailyIndicatorCountRepository, indicatorRepository);
+        try {
+            // TODO fix missing password on core
+            // generating indicators can cause errors
+            DailyIndicatorCountRepository dailyIndicatorCountRepository = ReportingLibrary.getInstance().dailyIndicatorCountRepository();
+            IndicatorQueryRepository indicatorQueryRepository = ReportingLibrary.getInstance().indicatorQueryRepository();
+            IndicatorRepository indicatorRepository = ReportingLibrary.getInstance().indicatorRepository();
+            reportIndicatorDao = new ReportIndicatorDaoImpl(indicatorQueryRepository, dailyIndicatorCountRepository, indicatorRepository);
+        } catch (Exception e) {
+            Timber.e(e);
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 }
