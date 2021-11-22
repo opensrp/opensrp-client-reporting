9521d239d4e25c0e3190ea0e241dc0985fc2af0c
vend <junaid.warsi@venturedive.com>
2021-11-22 18:24:22 +0500

    removed unused variable and imports

--- opensrp-reporting/src/test/java/org/smartregister/reporting/repository/DailyIndicatorCountRepositoryTest.java
+++ opensrp-reporting/src/test/java/org/smartregister/reporting/repository/DailyIndicatorCountRepositoryTest.java
@@ -6,7 +6,6 @@ import net.sqlcipher.Cursor;
 import net.sqlcipher.MatrixCursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
-import org.apache.tools.ant.taskdefs.Length;
 import org.hamcrest.collection.IsArrayWithSize;
 import org.junit.After;
 import org.junit.Assert;
@@ -110,7 +109,7 @@ public class DailyIndicatorCountRepositoryTest {
         Mockito.verify(dailyIndicatorCountRepositorySpy,Mockito.times(1)).getWritableDatabase();
         MatrixCursor matrixCursor = new MatrixCursor(new String[]{"id","value"}, 1);
         matrixCursor.addRow(new Object[]{"1","2.0"});
-        Cursor cursor = Mockito.doReturn(matrixCursor).when(sqLiteDatabase).rawQuery(ArgumentMatchers.anyString(), ArgumentMatchers.isNull(String[].class));
+        Mockito.doReturn(matrixCursor).when(sqLiteDatabase).rawQuery(ArgumentMatchers.anyString(), ArgumentMatchers.isNull(String[].class));
         Mockito.verify(sqLiteDatabase, Mockito.times(1))
                 .rawQuery(ArgumentMatchers.anyString(), ArgumentMatchers.isNull(String[].class));
 
